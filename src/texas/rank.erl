e012da3cd61c8de3a0849059b2ff6bb05a482af1
HpyHacking <crazyflapjack@gmail.com>
2011-10-24 23:55:08 +0800

    添加rank模块

--- src/texas/rank.erl
+++ src/texas/rank.erl
@@ -1,10 +1,22 @@
 -module(rank).
-
 -export([start/3]).
 
 -include("texas.hrl").
 
 start(Game, Ctx, _) ->
-  ?LOG([{rank_module, log}]),
+  Ranks = g:rank_hands(Game),
+  notify_hands(Game, Ranks),
   {stop, Game, Ctx}.
 
+notify_hands(_, []) ->
+  ok;
+
+notify_hands(Game, [H|T]) ->
+  Hand = hand:player_hand(H),
+  Event = #notify_hand{
+    player = H#hand.pid,
+    game = Game#game.gid,
+    hand = Hand
+  },
+  gen_server:cast(pp:id_to_player(H#hand.pid), Event),
+  notify_hands(Game, T).
