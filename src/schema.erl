e016830ac042d112acbb4255883ca3dc142c4c16
HpyHacking <crazyflapjack@gmail.com>
2011-11-11 10:03:18 +0800

    变更游戏启动参数

--- src/schema.erl
+++ src/schema.erl
@@ -149,7 +149,7 @@ populate() ->
           %8),
   g:setup(?GT_TEXAS_HOLDEM, 9,
           #limit{ type = ?LT_NO_LIMIT, low = 10, high = 20 },
-          5000, 15000 * 100, 1). 
+          3000, 15000, 1). 
   %g:setup(?GT_TEXAS_HOLDEM, 9,
           %#limit{ type = ?LT_NO_LIMIT, low = 10, high = 20 },
           %5000, ?PLAYER_TIMEOUT * 100, 
