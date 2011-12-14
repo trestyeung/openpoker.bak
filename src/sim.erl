c706b76da8b8e47b5fb8ca24ab2daaeb4b2b7fcb
HpyHacking <crazyflapjack@gmail.com>
2011-12-14 17:31:20 +0800

    让sim支持模拟玩家下注

--- src/sim.erl
+++ src/sim.erl
@@ -13,45 +13,50 @@ init() ->
   %Host = "192.168.1.102",
   Host = "127.0.0.1",
   Port = 8002,
+
   schema:install(),
+
   player:create("1010", "pass", "5b635bee55S3", "5Lit5Zu9", 1000), %% Jack
   player:create("1020", "pass", "6buRSkFDSw==", "6aaZ5riv", 1000), %% Sam 
   player:create("1030", "pass", "6buRSkFDSw==", "6aaZ5riv", 1000), %% Sam 
-
   player:update_photo(1, <<"def_face_1">>),
   player:update_photo(2, <<"def_face_2">>),
   player:update_photo(3, <<"def_face_3">>),
 
-  server:start(Host, Port).
+  server:start(Host, Port),
+  timer:sleep(2000),
+  login(),
 
-login() ->
-  login:login(<<"1010">>, <<"pass">>, self()),
-  login:login(<<"1020">>, <<"pass">>, self()),
-  login:login(<<"1030">>, <<"pass">>, self()),
   ok.
 
-socket() -> 
-  socket(players()).
+login() ->
+  login:login(<<"1010">>, <<"pass">>, spawn(?MODULE, player_loop, [1])),
+  login:login(<<"1020">>, <<"pass">>, spawn(?MODULE, player_loop, [2])),
+  login:login(<<"1030">>, <<"pass">>, spawn(?MODULE, player_loop, [3])),
 
-socket([]) ->
-  ok;
+  gen_server:cast(p(1), #player_query{player=p(1)}),
+  gen_server:cast(p(2), #player_query{player=p(2)}),
+  gen_server:cast(p(3), #player_query{player=p(3)}),
 
-socket([H|T]) ->
-  gen_server:cast(H, {'SOCKET', self()}),
-  socket(T).
+  ok.
 
-players() ->
-  players([1,2,3]).
+join(PID) ->
+  gen_server:cast(p(PID), #join{game=g(1), seat=0, amount=700.0}).
 
-players(L) ->
-  players(L, []).
-  
-players([], Acc) ->
-  Acc;
+call(PID) ->
+  gen_server:cast(p(PID), #raise{game=g(1), raise=0.0}).
 
-players([H|T], Acc) ->
-  [P] = db:read(tab_player, H),
-  players(T, [P#tab_player.process | Acc]).
+player_loop(PID) ->
+  receive
+    {packet, {notify_cancel_game, _}} ->
+      player_loop(PID);
+    {packet,{bet_req, _GID, _Call, _Min, _Max}} ->
+      call(PID),
+      player_loop(PID);
+    Msg ->
+      io:format("Flush [~p]: ~p~n", [PID, Msg]),
+      player_loop(PID)
+  end.
 
 p(PID) ->
   [P] = db:read(tab_player, PID),
@@ -61,50 +66,9 @@ g(GID) ->
   [G] = db:read(tab_game_xref, GID),
   G#tab_game_xref.process.
 
-dg(GID) when is_integer(GID) ->
-  dg(g(GID));
+debug(GID) when is_integer(GID) ->
+  debug(g(GID));
 
-dg(GID) when is_pid(GID) ->
+debug(GID) when is_pid(GID) ->
   gen_server:call(GID, 'DEBUG').
 
-watch() ->
-  gen_server:cast(p(1),#watch{game=g(1)}).
-
-join_two() -> 
-  login(),
-  timer:sleep(1000),
-  join(1),
-  join(2).
-
-join(P) ->
-  gen_server:cast(p(P), #join{game=g(1), seat=0, amount=100.0}).
-
-call(P) ->
-  gen_server:cast(p(P), #raise{game=g(1), raise=0.0}).
-
-rec(P) ->
-  receive 
-    {packet, M, {self, P}} -> 
-      M
-  end.
-
-flush() ->
-    flush(false).
-
-flush(Debug) ->
-  receive
-    X ->
-      if 
-        Debug ->
-          io:format("Flush: ~p~n", [X]);
-        true ->
-          ok
-      end,
-      flush()
-  after 0 ->
-      ok
-  end.
-
-rank(R) ->
-  Cards = hand:make_cards(R),
-  hand:player_hand(hand:rank(#hand{cards = Cards})).
